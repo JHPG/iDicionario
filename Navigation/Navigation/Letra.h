6d06e2ecb8dc848aa04bf2edbf533fd05bf43744
Jorge H. P. Garcia <j.hrique@hotmail.com>
2015-03-17 16:27:49 -0300

    Letra.h e .m trocado de pasta

---
+++ Navigation/Navigation/Letra.h
@@ -0,0 +1,20 @@
+//
+//  Letra.h
+//  Navigation
+//
+//  Created by Jorge Henrique P. Garcia on 3/16/15.
+//  Copyright (c) 2015 Jorge Henrique. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Letra : NSObject
+
+@property NSString *letra;
+@property UIImage *imagem;
+@property NSString *palavra;
+//Som
+
+-(instancetype)initWithLetra:(NSString*)letra andImagem:(UIImage*)imagem andPalavra:(NSString*)palavra; 
+
+@end
